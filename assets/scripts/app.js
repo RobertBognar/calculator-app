1659008fa650195e0d39ce4f8ed3c813dc7eac97
RobertBognar <robertbognar2@gmail.com>
2021-10-11 11:16:16 +0200

    updated app.js importing html attributes

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,4 +2,11 @@ const firstTheme = document.querySelector('#theme1');
 const secondTheme = document.querySelector('#theme2');
 const thirdTheme = document.querySelector('#theme3');
 const body = document.querySelector('body');
-
+const screen = document.getElementById('display');
+const secondScreen = document.getElementById('display-second');
+const displayInput = document.getElementById('display-input');
+const numberButtons = document.getElementById('btn-num');
+const symbolButtons = document.getElementById('btn-symb');
+const deleteButton = document.getElementById('btn-del');
+const resetButton = document.getElementById('btn-reset');
+const calculateButton = document.getElementById('calculate');
